6371a85cca40a7f933e1a9df8e5a5a2ff0597027
rmatovu987 <rmatovu987@gmail.com>
2022-05-11 19:13:32 +0300

    Task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ Write a function called filter_datum that returns the log message obfuscated
 import re
 from typing import List
 import logging
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 class RedactingFormatter(logging.Formatter):
@@ -33,3 +34,14 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr'{item}=.+?{separator}',
                          f'{item}={redaction}{separator}', message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """Returns logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
