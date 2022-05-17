8706792adcd2ddc4e4bc1233f8908d470f1f691f
rmatovu987 <rmatovu987@gmail.com>
2022-05-17 19:44:53 +0300

    Task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -42,7 +42,12 @@ class Auth:
         :param request:
         :return:
         """
-        return None
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
