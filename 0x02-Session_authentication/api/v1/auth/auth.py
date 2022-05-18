9f20deab8e586a24e2a89dc41c4346241374d458
rmatovu987 <rmatovu987@gmail.com>
2022-05-18 18:02:46 +0300

    Task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Class to manager authentication
+"""
+from typing import List, TypeVar
+
+from flask import request
+
+
+class Auth:
+    """
+    Auth Class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+
+        :param path:
+        :param excluded_paths:
+        :return:
+        """
+        if path is None:
+            return True
+        elif excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+
+        :param request:
+        :return:
+        """
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+
+        :param request:
+        :return:
+        """
+        return None
