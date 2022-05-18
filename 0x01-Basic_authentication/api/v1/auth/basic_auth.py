2f6d00776ab1f466fb784007513542a713b051a2
rmatovu987 <rmatovu987@gmail.com>
2022-05-18 05:46:05 +0300

    Task 6

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,18 @@ class BasicAuth(Auth):
     """
     Basic Auth Class
     """
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+
+        :param authorization_header:
+        :return:
+        """
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not authorization_header.startswith('Basic '):
+            return None
+        else:
+            return authorization_header.split(' ')[-1]
