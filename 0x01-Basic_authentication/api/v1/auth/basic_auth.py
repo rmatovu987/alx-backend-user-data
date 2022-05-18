e13dde46aa4af12ca01543569d6a08f008d0f52c
rmatovu987 <rmatovu987@gmail.com>
2022-05-18 05:58:11 +0300

    Task 8,9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,7 +30,8 @@ class BasicAuth(Auth):
             return authorization_header.split(' ')[-1]
 
     def decode_base64_authorization_header(self,
-                                           base64_authorization_header: str) -> str:
+                                           base64_authorization_header:
+                                           str) -> str:
         """
 
         :param base64_authorization_header:
