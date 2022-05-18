0780bfce7e958f7fd7c1ea5c2d0a005bdc8262d5
rmatovu987 <rmatovu987@gmail.com>
2022-05-18 06:00:23 +0300

    Task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,12 +48,12 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-        def extract_user_credentials(self,
-                                     decoded_base64_authorization_header:
-                                     str) -> (str, str):
-            """
-            Returns user email and password from Base64 decoded value
-            """
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        Returns user email and password from Base64 decoded value
+        """
         if decoded_base64_authorization_header is None:
             return (None, None)
         if not isinstance(decoded_base64_authorization_header, str):
@@ -65,21 +65,21 @@ class BasicAuth(Auth):
         return (email, password)
 
     def user_object_from_credentials(self, user_email: str,
-                                     user_pwd: str) -> TypeVar('User'):
-        """
-        Return a User instance based on email and password
-        """
-        if user_email is None or not isinstance(user_email, str):
-            return None
-        if user_pwd is None or not isinstance(user_pwd, str):
-            return None
-        try:
-            users = User.search({"email": user_email})
-            if not users or users == []:
+                                         user_pwd: str) -> TypeVar('User'):
+            """
+            Return a User instance based on email and password
+            """
+            if user_email is None or not isinstance(user_email, str):
                 return None
-            for u in users:
-                if u.is_valid_password(user_pwd):
-                    return u
-            return None
-        except Exception:
-            return None
+            if user_pwd is None or not isinstance(user_pwd, str):
+                return None
+            try:
+                users = User.search({"email": user_email})
+                if not users or users == []:
+                    return None
+                for u in users:
+                    if u.is_valid_password(user_pwd):
+                        return u
+                return None
+            except Exception:
+                return None
