c6a5dcad4590698a1a693c456f0bc6bdde23c4b0
rmatovu987 <rmatovu987@gmail.com>
2022-05-19 13:33:18 +0300

    Task 9

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,14 +63,14 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Not found handler
+    """ Request unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """forbidden handler
+    """ Request unauthorized handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
