4a217ea3e4b19af59580f9d18bdf6369ddb3863f
rmatovu987 <rmatovu987@gmail.com>
2022-05-24 08:07:40 +0300

    Task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,11 +34,14 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Method to add a user to db
-        :return:
+        Create a User object and save it to the database
+        Args:
+            email (str): user's email address
+            hashed_password (str): password hashed by bcrypt's hashpw
+        Return:
+            Newly created User object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
-
         return user
