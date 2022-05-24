ee45a4cee71dd9b69fec263f9fa1b31a9e0c11c2
rmatovu987 <rmatovu987@gmail.com>
2022-05-24 07:52:10 +0300

    Task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import *
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    The user class
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
