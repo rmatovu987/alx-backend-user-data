3c65ee07339a6e78d141c022b2e4ae6e4e1361af
rmatovu987 <rmatovu987@gmail.com>
2022-05-25 17:57:09 +0300

    Register User

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,9 +37,9 @@ class Auth:
         """
         try:
             self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        raise ValueError(f"User {email} already exists")
 
     def valid_login(self, email: str, password: str) -> bool:
         """Checks if a user's login details are valid.
